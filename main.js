05f011775bfe7f5afc47cedd7ddf360228203a0c
Ayetowo <90116741+Ayetowo@users.noreply.github.com>
2023-09-11 07:28:31 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+    const days = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    const d = new Date();
+    let month = days[d.getDay()];
+    document.getElementById("day").innerHTML = month;
+
+    const displayUTCTime = () => {
+        const utcTime = document.getElementById('time');
+        const currentTime = new Date().getTime();
+        utcTime.textContent = ` ${currentTime}`;
+    }
+
+    displayUTCTime();
+    setInterval(displayUTCTime, 1000);
